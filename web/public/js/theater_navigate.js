ba0e7b957e248eff52cceedc2b511a9a42cf5e1f
Ricardo Alcocer <alco@ricardoalcocer.com>
2016-07-25 16:21:41 -0700

    basic bootstrap and improvements in video rendering and navigation

--- web/public/js/theater_navigate.js
+++ web/public/js/theater_navigate.js
@@ -1,5 +1,13 @@
-function playvideo(vid){
+function playVideo(vid,still){
     var baseURL = 'http://localhost:9999/video/get_movie/';
     var vidplayer = document.getElementById('videoplayer');
     videojs(vidplayer).src({"type":"video/mp4", "src":baseURL + vid});
+    videojs(vidplayer).poster(still);
+}
+
+function setFirstVideo(vid,still){
+    var baseURL = 'http://localhost:9999/video/get_movie/';
+    var vidplayer = document.getElementById('videoplayer');
+    videojs(vidplayer).src({"type":"video/mp4", "src":baseURL + vid});
+    videojs(vidplayer).poster(still);
 }
